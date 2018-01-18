ba62ddfc10d8b539d6872a718ce2ec04b9217ccf
ElonKou <elonkou@ktime.cc>
2018-01-19 00:13:54 +0800

    Add the file which include 'some examples for using  EASYC' in the file named lib/Array.c

---
+++ lib/array/types.h
@@ -0,0 +1,9 @@
+#define OK 1			// 返回正常
+#define ERROR 0			// 返回错误
+#define OVERFLOW -1		// 返回溢出错误
+
+typedef int status;		// 函数返回类型
+
+typedef int Int;		
+typedef char Char;
+typedef char* String;
---
+++ test/hello.c
@@ -0,0 +1,15 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// FileName: hello.c
+// Author  : elonkou
+// E-mail  : elonkou@ktime.cc
+// Date    : Fri 19 Jan 2018 12:11:14 AM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char **argv){
+	printf("Hello,This is libibary for easy c!");
+    return 0;
+}
+
